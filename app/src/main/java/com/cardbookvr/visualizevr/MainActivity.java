dc32b65ef423bbf10b90eb94dbdda075ad2789fe
Jonathan Linowes <jonathan@parkerhill.com>
2016-02-06 12:32:06 -0500

    initial commit

---
+++ app/src/main/java/com/cardbookvr/visualizevr/MainActivity.java
@@ -0,0 +1,46 @@
+package com.cardbookvr.visualizevr;
+
+import android.os.Bundle;
+
+import com.cardbook.renderbox.IRenderBox;
+import com.cardbook.renderbox.RenderBox;
+import com.cardbook.renderbox.Transform;
+import com.cardbook.renderbox.components.Cube;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+public class MainActivity extends CardboardActivity implements IRenderBox {
+    private static final String TAG = "MainActivity";
+
+    CardboardView cardboardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView.setRenderer(new RenderBox(this, this));
+        setCardboardView(cardboardView);
+    }
+
+    @Override
+    public void setup() {
+        new Transform()
+                .setLocalPosition(0,0,-7)
+                .setLocalRotation(45,60,0)
+                .addComponent(new Cube(true));
+    }
+
+    @Override
+    public void preDraw() {
+
+    }
+
+    @Override
+    public void postDraw() {
+
+    }
+
+}
